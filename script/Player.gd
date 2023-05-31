2001d706f66af35e92beede831402a581f6c2285
GomuGomuNoBru <bruno.janacek@gmail.com>
2023-05-31 19:09:03 +0200

    'pushnotebook'

--- script/Player.gd
+++ script/Player.gd
@@ -4,6 +4,7 @@ export var speed = 20
 export var fall_acceleration = 75
 
 var velocity = Vector3.ZERO
+onready var anim = $"3DGodotRobot/AnimationPlayer"
 
 
 func _physics_process(delta):
@@ -11,17 +12,25 @@ func _physics_process(delta):
 
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
+		anim.play("Run-loop")
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
+		anim
 	if Input.is_action_pressed("move_back"):
 		direction.z += 1
+		anim
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
+		anim
 	if Input.is_key_pressed(KEY_ENTER):
 		get_tree().change_scene("res://Scenes/Menu.tscn")
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(translation + direction, Vector3.UP)
+	else:
+		anim.stop()
+	
+	
 
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
